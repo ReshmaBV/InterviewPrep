aa5d9b4c18a2727f56163b25379bf8c32b1e0615
ReshmaBV <37588515+ReshmaBV@users.noreply.github.com>
2026-02-16 21:15:04 +0530

    Implement reverse pyramid pattern in Java

---
+++ basic_patterns/reverse_pyramid.java
@@ -0,0 +1,24 @@
+
+//reverse pyramid
+
+class Solution {
+    public void pattern8(int n) {
+        int lastTerm = 1 + (n-1)*2;
+        for (int i = lastTerm; i >= 1; ) 
+        {
+            int emptyNo = (lastTerm - i)/2;
+            printPattern(emptyNo, " ");
+            printPattern(i, "*");
+            printPattern(emptyNo, " ");
+            i = i - 2;
+            System.out.print("\n");
+        }
+    }
+
+
+    void printPattern(int n, String symbol) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(symbol);
+        }
+    }
+}
